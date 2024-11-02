bdbfde9e823d3dd7fc98574f5e81fc3ab80c9bb9
Jey <126204426+Jeyrama@users.noreply.github.com>
2024-11-01 20:32:38 -0700

    Update Purchasing-a-vehicle.js

--- Purchasing-a-vehicle.js
+++ Purchasing-a-vehicle.js
@@ -44,3 +44,17 @@ Note:
 
 
 // Solution
+
+function nbMonths(startPriceOld, startPriceNew, savingperMonth, percentLossByMonth){
+  let months = 0, moneySaved = 0;
+	while (startPriceNew > startPriceOld + moneySaved){
+		moneySaved += savingperMonth;
+		startPriceOld -= (startPriceOld * (percentLossByMonth / 100));
+		startPriceNew -= (startPriceNew * (percentLossByMonth / 100));
+		months++;
+		if (months % 2 == 1){
+			percentLossByMonth += .5;
+		}
+	}
+	return [months, Math.round(startPriceOld + moneySaved - startPriceNew)];
+}
